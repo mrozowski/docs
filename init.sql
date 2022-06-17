ca5d848606597fb77a5531130b7aadc4612a1e95
Szymon Mrozowski <67066372+mrozowski@users.noreply.github.com>
2022-06-17 14:46:28 +0200

    Create init.sql

---
+++ init.sql
@@ -0,0 +1,7 @@
+CREATE TABLE IF NOT EXISTS countries (
+  COUNTRY_ID varchar(2) NOT NULL,
+  COUNTRY_NAME varchar(40) NOT NULL
+);
+
+INSERT INTO countries(COUNTRY_ID, COUNTRY_NAME)
+VALUES ('pl', 'Poland');
